1b2eba24e0f36ad14b653b80e4e5db32f0dc638c
Dennis Bartlett <bartlett.dc.1@gmail.com>
2020-06-15 05:40:41 -0400

    Initial commit

---
+++ overlay_server.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const chrome = require('selenium-webdriver/chrome');
+const {Builder} = require('selenium-webdriver');
+const express = require('express');
+const sass = require('node-sass');
+require('chromedriver');
+
+const app = express()
+const port = 65535
+
+const screen = {
+  width: 1280,
+  height: 720
+};
+
+let driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(new chrome.Options().headless().windowSize(screen).addArguments('--hide-scrollbars'))
+    .build();
+
+// Navigate to google.com, enter a search.
+driver.get(`http://127.0.0.1:${port}/overlay`);
+setInterval(() => {
+  driver.takeScreenshot().then(base64png => {
+    fs.writeFileSync('screenshot.png', new Buffer(base64png, 'base64'));
+  });
+}, 500);
+app.get('/overlay', (req, res) => res.sendFile(path.join(__dirname + '/overlay.html')));
+app.get('/styles.css', (req, res) => {
+  res.type('css');
+  res.send(sass.renderSync({
+    data: fs.readFileSync(path.join(__dirname + '/styles.scss')).toString()
+  }).css);
+});
+app.get('/overlay.js', (req, res) => res.sendFile(path.join(__dirname + '/overlay.js')));
+app.get('/screenshot.png', (req, res) => res.sendFile(path.join(__dirname + '/screenshot.png')));
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
